992d8574a05836340538eb69acae054d3cbf6a51
Your Name <you@example.com>
2019-08-06 14:25:24 +0800

    product-RichTextEditor 图片上传-富文本编辑器

--- src/pages/product/rich-text-editor.jsx
+++ src/pages/product/rich-text-editor.jsx
@@ -16,26 +16,30 @@ export default class RichTextEditor extends Component {
   }
 
   state = {
-    editorState: EditorState.createEmpty()  // 创建一个没有内容的编辑对象
+    editorState: EditorState.createEmpty() // 创建一个没有内容的编辑对象
   }
 
-//   显示已有的数据,即'修改'进来是有详情信息的
+  //   显示已有的数据,即'修改'进来是有详情信息的
   constructor(props) {
-    super(props);
-    const html = this.props.detail  //接受父组件传过来的detail,放到富文本里,还需进行判断
-    if(html){  //有值
-        const contentBlock = htmlToDraft(html)
-        if (contentBlock) {
-          const contentState = ContentState.createFromBlockArray(contentBlock.contentBlocks);
-          const editorState = EditorState.createWithContent(contentState);
-          this.state = {
-            editorState,
-          }
-        }
-    }else{  //没值
+    super(props)
+    const html = this.props.detail //接受父组件传过来的detail,放到富文本里,还需进行判断
+    if (html) {
+      //有值
+      const contentBlock = htmlToDraft(html)
+      if (contentBlock) {
+        const contentState = ContentState.createFromBlockArray(
+          contentBlock.contentBlocks
+        )
+        const editorState = EditorState.createWithContent(contentState)
         this.state = {
-            editorState: EditorState.createEmpty()  // 创建一个没有内容的编辑对象
-          }
+          editorState
+        }
+      }
+    } else {
+      //没值
+      this.state = {
+        editorState: EditorState.createEmpty() // 创建一个没有内容的编辑对象
+      }
     }
   }
 
@@ -50,6 +54,27 @@ export default class RichTextEditor extends Component {
     return draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
   }
 
+  // 图片上传
+  uploadImageCallBack = file => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest()
+      xhr.open("POST", "/manage/img/upload")
+      // xhr.setRequestHeader('Authorization', 'Client-ID XXXXX');  //不用设置请求头
+      const data = new FormData()
+      data.append("image", file)
+      xhr.send(data)
+      xhr.addEventListener("load", () => {
+        const response = JSON.parse(xhr.responseText) 
+        const url = response.data.url  //得到图片地址url 
+        resolve({data: {link: url}})
+      })
+      xhr.addEventListener("error", () => {
+        const error = JSON.parse(xhr.responseText)
+        reject(error)
+      })
+    })
+  }
+
   render() {
     const { editorState } = this.state
     return (
@@ -62,6 +87,18 @@ export default class RichTextEditor extends Component {
             paddingLeft: 10
           }}
           onEditorStateChange={this.onEditorStateChange}
+          /* 图片上传 */
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadImageCallBack,
+              alt: { present: true, mandatory: true }
+            }
+          }}
         />
         {/* <textarea
           disabled
